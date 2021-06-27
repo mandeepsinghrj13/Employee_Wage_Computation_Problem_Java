75de7edb70905f22b0e3aacc4efe248b268b362e
mandeep <singh.mr.1996@gmail.com>
2021-06-27 15:15:02 +0530

    Total Working Hours Or Days Is Reached For A Month

--- src/com/bridgelabz/EmployeeWageProblemJava.java
+++ src/com/bridgelabz/EmployeeWageProblemJava.java
@@ -5,31 +5,38 @@ public class EmployeeWageProblemJava
 
 	public static void main(String[] args) 
 	{
-		final int Part_Time = 1;
-        final int Full_Time = 2;
-        final int Wage_Per_hr = 20;
-        final int Working_Days = 20;
-   
-       int totalWage = 0;
-       for (int day = 1; day <= Working_Days; day++)
-       {
-           int empType = (int) (Math.random() * 10) % 3;
-           int Working_Hours =0;
-           switch (empType)
-           {
-               case Full_Time:
-                   Working_Hours = 8;
-                   break;
-               case Part_Time:
-                   Working_Hours = 4;
-                   break;
-               default:
-           }
-           int wage = Working_Hours * Wage_Per_hr;
-           System.out.println("Day " + day + " wage is:" + wage);
-           totalWage += wage;
-       }
-       System.out.println("Total wage for a month is " + totalWage);
+		int FullDayHr=8;
+		int PartTimeHr=4;
+		int WagePerHr=20;
+		int day=0;
+		int empHr=0;
+		int wage = 0;
+		while (day <= 19) {
+			int n = (int) (Math.random() * 10) % 3;
+		switch (n) {
+		case 1:
+			System.out.println("present fullday");
+			wage = (wage + (WagePerHr * FullDayHr));
+            empHr=(empHr+ FullDayHr);
+			day++;
+			break;
+		case 2:
+			System.out.println("present PartTime");
+			wage = (wage + (WagePerHr * PartTimeHr));
+			empHr=(empHr+ PartTimeHr);
+			day++;
+			break;
+		default:
+			System.out.println("absent");
+		    day++;
+			if( empHr == 100)
+				break;
+		}
+		 }
+		System.out.println("total wage of a month is");
+		System.out.println(wage);
+		System.out.println(empHr);
+		System.out.println(day);
 	}
 
 }
