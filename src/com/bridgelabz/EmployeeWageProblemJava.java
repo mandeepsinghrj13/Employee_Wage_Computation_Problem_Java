2e42e70ff059a6de9cbecfcf5adfdfd9da8b8351
mandeep <singh.mr.1996@gmail.com>
2021-06-27 14:57:23 +0530

    Add Part Time Employee And Wage

--- src/com/bridgelabz/EmployeeWageProblemJava.java
+++ src/com/bridgelabz/EmployeeWageProblemJava.java
@@ -5,15 +5,23 @@ public class EmployeeWageProblemJava
 
 	public static void main(String[] args) 
 	{
+		int PartTimeHours = 4;
 		int FullDay = 8;
 		int WageParHour = 20;
 		int empPresent = 1;
-		int empCheck = (int)Math.floor(Math.random() * 10) % 2;
+		int empPartTime = 2;
+		int empCheck = (int)Math.floor(Math.random() * 10) % 3;
 		if (empCheck == empPresent) 
 		{
 			int dailyWage = FullDay*WageParHour;
 			System.out.println("Employee is Present");
 			System.out.println("Employee Daily Wage Is : "+ dailyWage);
+		}
+		else if(empCheck == empPartTime)
+		{
+			int dailyWage = PartTimeHours*WageParHour;
+			System.out.println("Employee Part Time is Present");
+			System.out.println("Employee Part Time Wage Is : "+ dailyWage);
 		}
 		else 
 		{
