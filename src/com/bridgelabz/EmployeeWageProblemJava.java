f3ef8f48e4dbc15bca28e56f92f6ee0921271de9
mandeep <singh.mr.1996@gmail.com>
2021-06-27 14:53:59 +0530

    Calculate Daily Employee Wage

--- src/com/bridgelabz/EmployeeWageProblemJava.java
+++ src/com/bridgelabz/EmployeeWageProblemJava.java
@@ -5,11 +5,15 @@ public class EmployeeWageProblemJava
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Employee Wage Computation Problem On Java");
+		int FullDay = 8;
+		int WageParHour = 20;
+		int empPresent = 1;
 		int empCheck = (int)Math.floor(Math.random() * 10) % 2;
-		if (empCheck == 1) 
+		if (empCheck == empPresent) 
 		{
+			int dailyWage = FullDay*WageParHour;
 			System.out.println("Employee is Present");
+			System.out.println("Employee Daily Wage Is : "+ dailyWage);
 		}
 		else 
 		{
