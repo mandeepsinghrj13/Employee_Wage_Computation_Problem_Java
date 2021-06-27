296e05eb5ca19c6ed626727923833cbe1236aeae
mandeep <singh.mr.1996@gmail.com>
2021-06-27 15:10:55 +0530

    Calculateing Wages For A Month

--- src/com/bridgelabz/EmployeeWageProblemJava.java
+++ src/com/bridgelabz/EmployeeWageProblemJava.java
@@ -5,31 +5,31 @@ public class EmployeeWageProblemJava
 
 	public static void main(String[] args) 
 	{
-		int PartTimeHours = 4;
-		int FullDay = 8;
-		int WageParHour = 20;
-		final int empPresent = 1;
-		final int empPartTime = 2;
-		int dailyWage = 0;
-		int empCheck = (int)Math.floor(Math.random() * 10) % 3;
-		switch (empCheck) 
-		{
-		case empPresent:
-			dailyWage = FullDay*WageParHour;
-			System.out.println("Employee is Present");
-			System.out.println("Employee Daily Wage Is : "+ dailyWage);
-			break;
-		case empPartTime:
-		{
-			dailyWage = PartTimeHours*WageParHour;
-			System.out.println("Employee Part Time is Present");
-			System.out.println("Employee Part Time Wage Is : "+ dailyWage);
-			break;
-		}
-		default:
-			System.out.println("Employee is Absent");
-			break;
-		}
+		final int Part_Time = 1;
+        final int Full_Time = 2;
+        final int Wage_Per_hr = 20;
+        final int Working_Days = 20;
+   
+       int totalWage = 0;
+       for (int day = 1; day <= Working_Days; day++)
+       {
+           int empType = (int) (Math.random() * 10) % 3;
+           int Working_Hours =0;
+           switch (empType)
+           {
+               case Full_Time:
+                   Working_Hours = 8;
+                   break;
+               case Part_Time:
+                   Working_Hours = 4;
+                   break;
+               default:
+           }
+           int wage = Working_Hours * Wage_Per_hr;
+           System.out.println("Day " + day + " wage is:" + wage);
+           totalWage += wage;
+       }
+       System.out.println("Total wage for a month is " + totalWage);
 	}
 
 }
