5a37d16649d546e3c74e22152c09163bb1e45b0c
mandeep <singh.mr.1996@gmail.com>
2021-06-27 14:43:07 +0530

    Initialize Setup For Employee Wage Computation Problem Java

---
+++ src/com/bridgelabz/EmployeeWageProblemJava.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class EmployeeWageProblemJava 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Employee Wage Computation Problem On Java");
+	}
+
+}
