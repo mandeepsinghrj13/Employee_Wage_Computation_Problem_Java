892821de7a8939331c83a2a66ae8cebd57593fa8
mandeep <singh.mr.1996@gmail.com>
2021-06-28 15:01:58 +0530

    Compute Employee Wage For Mulitple Companies

--- src/com/bridgelabz/EmployeeWageProblemJava.java
+++ src/com/bridgelabz/EmployeeWageProblemJava.java
@@ -1,47 +1,48 @@
 package com.bridgelabz;
-
 public class EmployeeWageProblemJava 
 {
-
-	public static void calculateWages() 
+  public static void calculateWages(int FullDayHr, int PartTimeHr, 
+									  int WagePerHr,String companyName) 
 	{
-		int FullDayHr=8;
-		int PartTimeHr=4;
-		int WagePerHr=20;
 		int day=0;
 		int empHr=0;
 		int wage = 0;
-		while (day <= 19) {
+		while (day <= 19) 
+		{
 			int n = (int) (Math.random() * 10) % 3;
-		switch (n) {
+		switch (n) 
+		{
 		case 1:
-			System.out.println("present fullday");
+			//System.out.println("present fullday");
 			wage = (wage + (WagePerHr * FullDayHr));
             empHr=(empHr+ FullDayHr);
 			day++;
 			break;
 		case 2:
-			System.out.println("present PartTime");
+			//System.out.println("present PartTime");
 			wage = (wage + (WagePerHr * PartTimeHr));
 			empHr=(empHr+ PartTimeHr);
 			day++;
 			break;
 		default:
-			System.out.println("absent");
+			//System.out.println("absent");
 		    day++;
 			if( empHr == 100)
 				break;
 		}
-		 }
-		System.out.println("total wage of a month is");
-		System.out.println(wage);
-		System.out.println(empHr);
-		System.out.println(day);
+		}
+		System.out.println("Calculate Wages For :" + companyName);
+		System.out.println("EmpHr :"+empHr);
+		System.out.println("Day :"+day);
+		System.out.println("Total Wage is :"+wage);
+		System.out.println(" ");
 	}
 	
 	public static void main(String[] args) 
 	{
-		calculateWages();
+		calculateWages(8, 2, 20, "Dmart");
+		calculateWages(8, 4, 20, "Reliance Digital");
+		calculateWages(8, 6, 20, "Patrol Pump");
 	}
 
 }
