c2fec39ffb02888a840d8197afb70f2b65aa4b65
mandeep <singh.mr.1996@gmail.com>
2021-06-29 21:33:12 +0530

    Save the total eage for each company

--- src/com/bridgelabz/EmployeeWageProblemJava.java
+++ src/com/bridgelabz/EmployeeWageProblemJava.java
@@ -1,48 +1,104 @@
 package com.bridgelabz;
 public class EmployeeWageProblemJava 
 {
-  public static void calculateWages(int FullDayHr, int PartTimeHr, 
-									  int WagePerHr,String companyName) 
-	{
-		int day=0;
-		int empHr=0;
-		int wage = 0;
-		while (day <= 19) 
-		{
-			int n = (int) (Math.random() * 10) % 3;
-		switch (n) 
-		{
-		case 1:
-			//System.out.println("present fullday");
-			wage = (wage + (WagePerHr * FullDayHr));
-            empHr=(empHr+ FullDayHr);
-			day++;
-			break;
-		case 2:
-			//System.out.println("present PartTime");
-			wage = (wage + (WagePerHr * PartTimeHr));
-			empHr=(empHr+ PartTimeHr);
-			day++;
-			break;
-		default:
-			//System.out.println("absent");
-		    day++;
-			if( empHr == 100)
-				break;
-		}
-		}
-		System.out.println("Calculate Wages For :" + companyName);
-		System.out.println("EmpHr :"+empHr);
-		System.out.println("Day :"+day);
-		System.out.println("Total Wage is :"+wage);
-		System.out.println(" ");
-	}
-	
-	public static void main(String[] args) 
-	{
-		calculateWages(8, 2, 20, "Dmart");
-		calculateWages(8, 4, 20, "Reliance Digital");
-		calculateWages(8, 6, 20, "Patrol Pump");
-	}
+	int IsFullTime;
+    int IsPartTime;
+   private  int wagePerHr;
+   private final int fullDayHr;
+   private final int partTimeHr;
+   private final int empHr;
+   // int dailyEmpWage;
+
+   public EmployeeWageProblemJava(int isFullTime, int isPartTime, int wagePerHr, int fullDayHr, int partTimeHr, int empHr, String company) {
+       IsFullTime = isFullTime;
+       IsPartTime = isPartTime;
+       this.wagePerHr = wagePerHr;
+       this.fullDayHr = fullDayHr;
+       this.partTimeHr = partTimeHr;
+       this.empHr = empHr;
+   }
+
+   public  void computeWage(String company){
+       this.setWagePerHr(company);
+
+       int dailyEmpWage=0;
+       int day = 0;
+
+       double empCheck = Math.floor(Math.random() * 10) % 3;
+       System.out.println(empCheck);
+       while (day <= 19) {
+
+           int n =(int)(Math.random() * 10) % 3;
+           switch (n) {
+               case 1:
+                   System.out.println("present fullday");
+                   dailyEmpWage = (dailyEmpWage + (wagePerHr * fullDayHr));
+
+                   day++;
+                   break;
+               case 2:
+                   System.out.println("present PartTime");
+                   dailyEmpWage = (dailyEmpWage + (wagePerHr * partTimeHr));
+
+
+                   day++;
+                   break;
+               default:
+                   System.out.println("Employee is absent. ");
+                   dailyEmpWage = (dailyEmpWage + (wagePerHr * empHr));
+                   day++;
+
+                   if( empHr== 100)
+                       break;
+           }
+       }
+       System.out.println("total wage of a month is::" + dailyEmpWage);
+
+
+   }
+
+   private void setWagePerHr(String company) {
+
+       switch (company) {
+           case "A":
+               this.wagePerHr = 10;
+               break;
+           case "B":
+               this.wagePerHr = 100;
+               break;
+           case "C":
+               this.wagePerHr = 1000;
+               break;
+           case "D":
+               this.wagePerHr = 10000;
+               break;
+
+           default:
+               this.wagePerHr = 5;
+               break;
+       }
+   }
+
+   public static void main(String[] args) {
+       System.out.println("Welcome to Employee Wage Computation....!!!");
+
+
+       System.out.println("This is for compony A::");
+       EmployeeWageProblemJava e1= new EmployeeWageProblemJava(100,100,100,100,100,7,"A");
+       e1. computeWage("A");
+
+       System.out.println("This is for compony B::");
+       EmployeeWageProblemJava e2= new EmployeeWageProblemJava(100,100,100,100,100,7, "B");
+       e2.computeWage("B");
+
+       System.out.println("This is for compony C::");
+       EmployeeWageProblemJava e3= new EmployeeWageProblemJava(100,100,100,100,100,7, "C");
+       e3.computeWage("C");
+
+       System.out.println("This is for compony D::");
+       EmployeeWageProblemJava e4= new EmployeeWageProblemJava(100,100,100,100,100,7, "D");
+       e4.computeWage("D");
+
+   }
 
 }
